d4acccc86259922efbe2fc079bcc2f6db0a74846
DrFace <praveen.liyanage@axcertro.com>
2025-10-01 12:29:44 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -12,7 +13,7 @@ export default defineConfig({
         react(),
     ],
     build: {
-        outDir: 'dist', // <-- this is the folder Vercel expects
+        outDir: 'dist',
         rollupOptions: {
             input: {
                 main: path.resolve(__dirname, 'resources/js/app.tsx'),
